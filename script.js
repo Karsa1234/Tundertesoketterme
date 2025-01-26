79e51fb7f1022e393e19a7ba6498bc00e1a5306f
Karsa <razsikarsa2010@gmail.com>
2025-01-26 11:19:04 +0100

    Étterem

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById("navigateBtn").addEventListener("click", function() {
-        window.location.href = "étlap.html";
+        window.location.href = "index2.html";
 });
 document.getElementById("navigateBtn2").addEventListener("click", function() {
-    window.location.href = "Helyszín,kapcsolat.html";
+    window.location.href = "index3.html";
 });
 document.getElementById("navigateBtn3").addEventListener("click", function() {
-    window.location.href = "Képek,Videók.html";
+    window.location.href = "index4.html";
 });
 //<h2>Köszöntelek a Tündértesók éttermébe!</h2>
 //<h2>Az árakat kérdezd a felszolgálótól(nem lesz több, mint 200Ft)</h2>
